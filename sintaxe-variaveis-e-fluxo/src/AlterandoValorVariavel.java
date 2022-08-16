60827e957e1b14d4cfda027a426a0d84fb81cd79
eddiex79 <eddiex79@gmail.com>
2022-08-16 20:40:14 -0300

    first commit

---
+++ sintaxe-variaveis-e-fluxo/src/AlterandoValorVariavel.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class aVistaOuAPrazo {
+Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("--------Tipos de Pagamentos--------");
+		
+		System.out.print("Digite o valor do produto");
+		Double pagamento = aVista;
+	}
+}
